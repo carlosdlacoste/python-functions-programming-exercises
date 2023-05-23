8eb3f2938b6de59679f09a0cb995732c7b7f0d65
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-05-23 15:43:54 +0000

    Functions exercises finished

--- exercises/08-Function-that-returns/app.py
+++ exercises/08-Function-that-returns/app.py
@@ -5,3 +5,6 @@ def euro_to_yen(euro_value):
 	return euro_value * 124.15
 
 ####### ↓ YOUR CODE BELOW ↓ #######
+euro_value = dollar_to_euro(137)
+yen_value = euro_to_yen(euro_value)
+print(yen_value)
